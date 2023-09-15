f2e4e3a7550f1482d261bed6cacd66a57d24ef3c
psj <142005747+gyeongeunpark@users.noreply.github.com>
2023-09-15 16:00:27 +0900

    Initial commit

---
+++ 09.react_web_back/src/main/java/kr/or/iei/member/controller/MemberController.java
@@ -0,0 +1,11 @@
+package kr.or.iei.member.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //전부 responseBody 적용(모두 비동기로 리턴)
+@RequestMapping(value = "/member")
+public class MemberController {
+	
+	
+}
